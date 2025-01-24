77b199a2db4cc6d4128162ab4bb5708608529406
Tym Phillips <tymphillips@gmail.com>
2025-01-24 11:25:14 -0600

    Add forRootAsync

--- src/lyric.module.ts
+++ src/lyric.module.ts
@@ -4,6 +4,12 @@ import { LyricConfig } from './types';
 
 export const LYRIC_CONFIG = 'LYRIC_CONFIG';
 
+export interface LyricModuleAsyncOptions {
+  useFactory: (...args: any[]) => Promise<LyricConfig> | LyricConfig;
+  inject?: any[];
+  imports?: any[];
+}
+
 @Module({})
 export class LyricModule {
   static forRoot(config: LyricConfig): DynamicModule {
@@ -21,6 +27,28 @@ export class LyricModule {
     return {
       module: LyricModule,
       providers: [configProvider, clientProvider],
+      exports: [LyricClient],
+      global: true,
+    };
+  }
+
+  static forRootAsync(options: LyricModuleAsyncOptions): DynamicModule {
+    const clientProvider: Provider = {
+      provide: LyricClient,
+      useFactory: async (config: LyricConfig) => new LyricClient(config),
+      inject: [LYRIC_CONFIG],
+    };
+
+    const asyncConfigProvider: Provider = {
+      provide: LYRIC_CONFIG,
+      useFactory: options.useFactory,
+      inject: options.inject || [],
+    };
+
+    return {
+      module: LyricModule,
+      imports: options.imports || [],
+      providers: [asyncConfigProvider, clientProvider],
       exports: [LyricClient],
       global: true,
     };
